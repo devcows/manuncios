17897fa5317aa8bdfdf207491dbd973482cfb173
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2014-10-22 22:50:51 +0200

    resolve #24

--- app/src/main/java/com/app/first/milanuncios/OfferGetTask.java
+++ app/src/main/java/com/app/first/milanuncios/OfferGetTask.java
@@ -6,6 +6,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,16 +27,43 @@ public class OfferGetTask extends AsyncTask<OfferTaskListener, Void, Offer> {
     @Override
     protected Offer doInBackground(OfferTaskListener... listeners) {
         this.listeners = listeners;
-
         List<String> images = new ArrayList<String>();
-        Document doc = Utils.getDocumentFromUrl(offer.getUrl());
-        if (doc != null) {
-            Elements rows = doc.select("div.pagAnuFoto img");
-            for (Element row : rows) {
-                String strImage = row.attr("src");
-                images.add(strImage);
+
+        int index = 1;
+        boolean finish = false;
+
+        while(!finish){
+            try {
+                String idOffer = offer.getId();
+
+                String strUrl = "http://91.229.239.8/fg/" + idOffer.substring(0, 4) + "/" + idOffer.substring(4, 6) + "/" + idOffer + "_" + index + ".jpg";
+                URL url = new URL(strUrl);
+                HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+
+                huc.setRequestMethod("HEAD");
+                huc.connect();
+                int code = huc.getResponseCode();
+                if(code >= 200 && code <300){
+                    images.add(strUrl);
+                } else{
+                    finish = true;
+                }
+
+                index++;
+            }catch (IOException ex){
+                finish = true;
             }
         }
+
+        /////// Old mode not working now.
+//        Document doc = Utils.getDocumentFromUrl(offer.getUrl());
+//        if (doc != null) {
+//            Elements rows = doc.select("div.pagAnuFoto img");
+//            for (Element row : rows) {
+//                String strImage = row.attr("src");
+//                images.add(strImage);
+//            }
+//        }
 
         offer.setSecondaryImages(images);
         return offer;
