9f24d9d6b638f3852005d9c7dc422be3f6665203
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2014-10-25 01:07:38 +0200

    Version in main activity.

--- app/src/main/java/com/devcows/manuncios/MainActivity.java
+++ app/src/main/java/com/devcows/manuncios/MainActivity.java
@@ -2,9 +2,11 @@ package com.devcows.manuncios;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity implements MainTaskListener {
@@ -13,6 +15,19 @@ public class MainActivity extends Activity implements MainTaskListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String version = "";
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            if(pInfo.versionName != null && !pInfo.versionName.isEmpty()){
+                version = "Version: " + pInfo.versionName;
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        TextView txtVersion = (TextView) findViewById(R.id.txtVersion);
+        txtVersion.setText(version);
 
         MainGetTask mainGetTask = new MainGetTask(this);
         mainGetTask.execute(this);
